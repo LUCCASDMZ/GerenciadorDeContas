a02f89fbcfc6654f946e7011a5d13866bbc260cc
LUCCAS <105461563+LUCCASDMZ@users.noreply.github.com>
2024-07-30 16:23:49 -0300

    css

--- resources/views/contas/index.blade.php
+++ resources/views/contas/index.blade.php
@@ -1,15 +1,54 @@
 @extends('layouts.admin')
 @section('content')
 
+<div class="card mt-4 mb-4 border-light shadow">
+    <div class="card-header d-flex justify-content-between">
+        <span> Listar Contas</span>
+        <span>
+            <a href="{{ route('conta.create')}}">
+                <button class="btn btn-success btn-sm">Cadastrar</button><br>
+            </a>
+        </span>
+    </div>
 
-    <a href="{{ route('conta.create')}}">
-        <button>Cadastrar</button><br>
-    </a><br>
-    <a href=" {{route('site.menu')}} ">
-        <button>Voltar</button>
-    </a>
+    @forelse ($contas as $conta)
 
-    <h1>Listar as contas</h1>
+        <div class="card-body">
+            <table class="table">
+                <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Valor</th>
+                    <th scope="col">Vencimento</th>
+                    <th scope="col">Ações</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row">{{$conta->id}}</th>
+                    <td>{{$conta->nome}}</td>
+                    <td>R$ {{ number_format($conta->valor, 2, ',', '.') }}</td>
+                    <td>{{ \Carbon\Carbon::parse($conta->vencimento)->format('d/m/Y') }}</td>
+                    <td>
+                        <a class="btn btn-primary" href="{{route('conta.show', ['conta' => $conta->id])}}">Visualizar</a>
+                        <a class="btn btn-warning" href="{{route('conta.edit', ['conta' => $conta->id])}}">Editar</a>
+                        <form action="{{route('conta.destroy', ['conta' => $conta->id])}}" method="post">
+                            @csrf
+                            @method('delete')
+                            <button class="btn btn-danger" type="submit" onclick="return confirm('Tem certeza que deseja apagar esse registro?'">Apagar</button>
+                        </form>
+                    </td>
+                </tr>
+                </tbody>
+                </table>
+        </div>
+    @empty
+        <span style="color: #f00">
+            Nenhuma conta encontrada!
+        </span>
+    @endforelse
+    </div>
 
         {{-- Verificar se existe a sessão success e imprime o valor --}}
         @if (session('success'))
@@ -17,34 +56,4 @@
             {{session('success')}}
         </span><br><br>
         @endif
-
-    @forelse ($contas as $conta)
-    ID: {{ $conta->id }} <br>
-    Nome: {{ $conta->nome }} <br>
-    Valor: R$ {{ number_format($conta->valor, 2, ',', '.') }} <br>
-    Vencimento: {{ \Carbon\Carbon::parse($conta->vencimento)->format('d/m/Y') }} <br><br>
-
-    <a href="{{ route('conta.show', ['conta' => $conta->id ])}}">
-        <button>Visualizar</button><br>
-    </a><br>
-    <a href="{{ route('conta.edit', ['conta' => $conta->id])}}">
-        <button>Editar</button><br>
-    </a><br>
-
-
-    <form action="{{ route('conta.destroy', ['conta' => $conta->id])}}" method="post">
-        @csrf
-        @method('delete')
-        <button type="submit" onclick="return confirm('Tem certeza que deseja apagar esse registro?')">Apagar</button><br>
-    </form>
-
-
-    <br><hr>
-    @empty
-        <span style="color: #f00">
-            Nenhuma conta encontrada!
-        </span>
-
-    @endforelse
-
-    @endsection
+@endsection
